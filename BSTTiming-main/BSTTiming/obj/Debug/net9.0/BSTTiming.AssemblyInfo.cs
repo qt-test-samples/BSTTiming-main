a3a0209c1920479ba79611d766e9465076a4c538
NoReply Amazon <no-reply@amazon.com>
2024-11-18 09:59:48 +0000

    [Commit] Iteration 1142 #errors = 145 <= 145.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/BSTTiming-main/BSTTiming/Program.cs][CS0246][15,21] The type or namespace name 'System' could not be found (are you missing a using directive or an assembly reference?)``.

--- BSTTiming-main/BSTTiming/obj/Debug/net9.0/BSTTiming.AssemblyInfo.cs
+++ BSTTiming-main/BSTTiming/obj/Debug/net9.0/BSTTiming.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3253ffce67649f357a82df02db45cd81f899d8db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a007a4d9b60c45fbb8fc0e6a81d5a269eb4284d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
