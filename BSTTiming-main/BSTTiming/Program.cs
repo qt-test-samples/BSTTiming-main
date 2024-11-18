2fc880245876bf24235dcf004dff0617e4731cb2
NoReply Amazon <no-reply@amazon.com>
2024-11-18 10:34:00 +0000

    Revert "[Commit] Iteration 1233 #errors = 148 <= 148."
    This reverts commit f5eb380415b5264a3c415e19bc90510f9930f77f.

--- BSTTiming-main/BSTTiming/Program.cs
+++ BSTTiming-main/BSTTiming/Program.cs
@@ -54,7 +54,7 @@ using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus
             int[] items = generateSearchItems(1024);
 
             // Create a stopwatch
-            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            Stopwatch sw = new Stopwatch();
 
             Random random = new Random();
 
@@ -141,9 +141,9 @@ using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus
         /// <summary>
         /// Returns the number of milliseconds that have elapsed on the Stopwatch.
         /// </summary>
-        public static double msecs(System.Diagnostics.Stopwatch sw)
+        public static double msecs(Stopwatch sw)
         {
-            return (((double)sw.ElapsedTicks) / System.Diagnostics.Stopwatch.Frequency) * 1000;
+            return (((double)sw.ElapsedTicks) / Stopwatch.Frequency) * 1000;
         }
     }
 }
