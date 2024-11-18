2d9806f22edbfaa3edc313f361ebcd07d1c4517b
NoReply Amazon <no-reply@amazon.com>
2024-11-18 09:53:44 +0000

    [Commit] Iteration 1124 #errors = 143 <= 142.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/BSTTiming-main/BSTTiming/Program.cs][CS0103][33,33] The name 'Math' does not exist in the current context``.

--- BSTTiming-main/BSTTiming/obj/Debug/net9.0/BSTTiming.AssemblyInfo.cs
+++ BSTTiming-main/BSTTiming/obj/Debug/net9.0/BSTTiming.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dfca1da9c89388283d9af13d994800b11655b06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a1dd48230949315c8bb32afed87dcfa5bef596a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
