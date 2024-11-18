b79ada882d89f5d2a46258c5a8331ec66f92cabc
NoReply Amazon <no-reply@amazon.com>
2024-11-18 11:20:49 +0000

    [Commit] Iteration 1350 #errors = 149 <= 148.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/BSTTiming-main/BSTTiming/Program.cs][CS0103][32,17] The name 'Console' does not exist in the current context``.

--- BSTTiming-main/BSTTiming/obj/Debug/net9.0/BSTTiming.AssemblyInfo.cs
+++ BSTTiming-main/BSTTiming/obj/Debug/net9.0/BSTTiming.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21baf66482abb2092c4d76951e16ccf69a9178d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd08ffeddc9c7700b5a81687f8b4b97cb5575785")]
 [assembly: System.Reflection.AssemblyProductAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
