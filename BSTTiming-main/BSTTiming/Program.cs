adfd69918a88d8badb4dbfb24a47f96946278468
NoReply Amazon <no-reply@amazon.com>
2024-11-18 08:27:57 +0000

    [Commit] Iteration 921 #errors = 141 <= 140.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/BSTTiming-main/BSTTiming/Program.cs][CS0518][23,42] Predefined type 'System.Object' is not defined or imported``.

--- BSTTiming-main/BSTTiming/Program.cs
+++ BSTTiming-main/BSTTiming/Program.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 
 namespace BSTTiming
 {
-    class Program : System.Object
+    class Program
     {
         /// <summary>
         /// Duration of one second
