db868ac57b258216c38a58c15d8da67af4e64328
NoReply Amazon <no-reply@amazon.com>
2024-11-18 04:33:02 +0000

    Revert "[Commit] Iteration 327 #errors = 175 <= 147."
    This reverts commit 14ff37a02bf1aa43e9a549815f0bc6fc809a6892.

--- BSTTiming-main/BSTTiming/Program.cs
+++ BSTTiming-main/BSTTiming/Program.cs
@@ -20,22 +20,23 @@ namespace BSTTiming
 
         public static System.Int32 SIZE;
 
-        static void Main(System.String[] args)
+        static void Main(string[] args)
         {
 
-            System.String line;
-using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus Zarate\Desktop\timingResults.txt"))
+            string line;
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus Zarate\Desktop\timingResults.txt"))
             {
                 line = "Time";
-                System.Console.WriteLine(line);
+                Console.WriteLine(line);
                 file.WriteLine(line);
 
-                for (System.Int32 i = 10; i <= 20; i++)
+                for (int i = 10; i <= 20; i++)
                 {
-                    SIZE = (System.Int32)System.Math.Pow(2, i);
-                    line = RunBSTTiming(SIZE).ToString();
+                    SIZE = (int)Math.Pow(2, i);
+                    line = RunBSTTiming(SIZE) + "";
 
-                    System.Console.WriteLine(line);
+                    // Uncomment me
+                    Console.WriteLine(line);
                     file.WriteLine(line);
                 }
             }
@@ -43,29 +44,29 @@ using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus
             Console.Read();
         }
 
-        public static System.Double RunBSTTiming(System.Int32 size)
+        public static System.Double RunBSTTiming(int size)
         {
-            // Construct a randomly-generated balanced
+            // Construct a randomly-generated balanced 
             //binary search tree
-            System.Collections.Generic.SortedSet<System.Int32> bst = generateTree(size);
+            SortedSet<int> bst = generateTree(size);
 
-            System.Int32[] items = generateSearchItems(1024);
+            int[] items = generateSearchItems(1024);
 
             // Create a stopwatch
-            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            Stopwatch sw = new Stopwatch();
 
-            System.Random random = new System.Random();
+            Random random = new Random();
 
             // Keep increasing the number of repetitions until one second elapses.
-            System.Double elapsed = 0;
-            System.Int64 repetitions = 1;
+            double elapsed = 0;
+            long repetitions = 1;
             do
             {
                 repetitions *= 2;
                 sw.Restart();
-                for (System.Int32 i = 0; i < repetitions; i++)
+                for (int i = 0; i < repetitions; i++)
                 {
-                    for (System.Int32 elt = 0; elt < 1024; elt++)
+                    for (int elt = 0; elt < 1024; elt++)
                     {
                         bst.Contains(items[elt]);
                     }
@@ -73,10 +74,10 @@ using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus
                 sw.Stop();
                 elapsed = msecs(sw);
             } while (elapsed < DURATION);
-            System.Double totalAverage = elapsed / repetitions;
+            double totalAverage = elapsed / repetitions;
 
             // Create a stopwatch
-            sw = new System.Diagnostics.Stopwatch();
+            sw = new Stopwatch();
 
             // Keep increasing the number of repetitions until one second elapses.
             elapsed = 0;
@@ -85,27 +86,27 @@ using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus
             {
                 repetitions *= 2;
                 sw.Restart();
-                for (System.Int32 i = 0; i < repetitions; i++)
+                for (int i = 0; i < repetitions; i++)
                 {
-                    for (System.Int32 elt = 0; elt < 1024; elt++)
+                    for (int elt = 0; elt < 1024; elt++)
                     {
                     }
                 }
                 sw.Stop();
                 elapsed = msecs(sw);
             } while (elapsed < DURATION);
-            System.Double overheadAverage = elapsed / repetitions;
-
+            double overheadAverage = elapsed / repetitions;
+            
             // Return the difference, averaged over size
             return (totalAverage - overheadAverage) / 1024;
         }
 
-        private static System.Int32[] generateSearchItems(System.Int32 size)
+        private static int[] generateSearchItems(int size)
         {
-            System.Collections.Generic.HashSet<System.Int32> set = new System.Collections.Generic.HashSet<System.Int32>();
-            System.Random random = new System.Random();
-            System.Int32 num;
-            for(System.Int32 i = 0; i < size; i++)
+            HashSet<int> set = new HashSet<int>();
+            Random random = new Random();
+            int num;
+            for(int i = 0; i < size; i++)
             {
                 do
                 {
@@ -117,13 +118,13 @@ using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus
             return set.ToArray();
         }
 
-        private static System.Collections.Generic.SortedSet<System.Int32> generateTree(System.Int32 size)
+        private static SortedSet<int> generateTree(int size)
         {
-            System.Collections.Generic.SortedSet<System.Int32> bst = new System.Collections.Generic.SortedSet<System.Int32>();
-            System.Random random = new System.Random();
+            SortedSet<int> bst = new SortedSet<int>();
+            Random random = new Random();
 
-            System.Int32 number;
-            for (System.Int32 i = 0; i < size; i++)
+            int number;
+            for (int i = 0; i < size; i++)
             {
                 do
                 {
@@ -132,16 +133,16 @@ using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus
 
                 bst.Add(number);
             }
-
+            
             return bst;
         }
 
         /// <summary>
         /// Returns the number of milliseconds that have elapsed on the Stopwatch.
         /// </summary>
-        public static System.Double msecs(System.Diagnostics.Stopwatch sw)
+        public static double msecs(Stopwatch sw)
         {
-            return (((System.Double)sw.ElapsedTicks) / System.Diagnostics.Stopwatch.Frequency) * 1000;
+            return (((double)sw.ElapsedTicks) / Stopwatch.Frequency) * 1000;
         }
 
     }
