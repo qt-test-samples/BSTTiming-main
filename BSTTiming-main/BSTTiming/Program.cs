3e0569afbba7b19d7a887a962c6dd6a0795e3ce2
NoReply Amazon <no-reply@amazon.com>
2024-11-18 13:10:53 +0000

    Revert "[Commit] Iteration 1627 #errors = 141 <= 141."
    This reverts commit 36da7bc32847ac20ca9b838fdc1156a4feb8ff80.

--- BSTTiming-main/BSTTiming/Program.cs
+++ BSTTiming-main/BSTTiming/Program.cs
@@ -6,7 +6,7 @@ using System.Linq;
 
 namespace BSTTiming
 {
-    class Program
+    class Program : System.Object
     {
         /// <summary>
         /// Duration of one second
@@ -38,7 +38,7 @@ using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus
             Console.Read();
         }
 
-        public static double RunBSTTiming(int size)
+        public static System.Double RunBSTTiming(int size)
         {
             // Construct a randomly-generated balanced
             //binary search tree
