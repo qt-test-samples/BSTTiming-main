3b495d3759f9d7d218f4a5b0932dddd3a949fc69
NoReply Amazon <no-reply@amazon.com>
2024-11-18 10:07:07 +0000

    [Commit] Iteration 1161 #errors = 145 <= 144.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/BSTTiming-main/BSTTiming/Program.cs][CS0518][45,43] Predefined type 'System.Int32' is not defined or imported``.

--- BSTTiming-main/BSTTiming/obj/Debug/net9.0/BSTTiming.AssemblyInfo.cs
+++ BSTTiming-main/BSTTiming/obj/Debug/net9.0/BSTTiming.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51948f304035a780c10ec74badd509d7dede7e79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3abf5d4b98690ed41a87acd9960720fca3f43200")]
 [assembly: System.Reflection.AssemblyProductAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
