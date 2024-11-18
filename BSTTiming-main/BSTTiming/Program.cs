53d40774e28e7d3640bd3fb4c62a15c47d059f74
NoReply Amazon <no-reply@amazon.com>
2024-11-18 10:31:45 +0000

    Revert "[Commit] Iteration 1227 #errors = 141 <= 148."
    This reverts commit 6db6483738d0aa97b0a1b28e278a9281f62c65d3.

--- BSTTiming-main/BSTTiming/Program.cs
+++ BSTTiming-main/BSTTiming/Program.cs
@@ -3,17 +3,24 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Globalization;
+using System.Runtime;
 
 namespace BSTTiming
 {
-    class Program
+    class Program : System.Object
     {
         /// <summary>
         /// Duration of one second
         /// </summary>
         public const int DURATION = 1000;
 
-        public static int SIZE = 0;
+        public static System.Int32 SIZE = 0;
 
         static void Main(string[] args)
         {
