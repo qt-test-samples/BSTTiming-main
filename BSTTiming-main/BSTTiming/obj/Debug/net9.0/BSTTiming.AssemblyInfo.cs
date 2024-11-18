74f20cea6dcb7fa6f854d66583d9a5d7d08e8e37
NoReply Amazon <no-reply@amazon.com>
2024-11-18 05:46:54 +0000

    [Commit] Iteration 524 #errors = 148 <= 147.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/BSTTiming-main/BSTTiming/Program.cs][CS0518][25,16] Predefined type 'System.Void' is not defined or imported``.

--- BSTTiming-main/BSTTiming/obj/Debug/net9.0/BSTTiming.AssemblyInfo.cs
+++ BSTTiming-main/BSTTiming/obj/Debug/net9.0/BSTTiming.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89b0ffeb6b661900a1cc31931c5b47cd5c89d324")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdc3455a1c2e3cd73a64b591e1eac6bb76a1d77a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
