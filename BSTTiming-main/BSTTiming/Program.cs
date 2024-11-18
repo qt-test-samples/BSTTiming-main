d1644e896b54c6efef61ebe7fa8b946bfa8474ed
NoReply Amazon <no-reply@amazon.com>
2024-11-18 12:30:33 +0000

    Revert "[Commit] Iteration 1527 #errors = 136 <= 135."
    This reverts commit 3333039f2f849e7ebe0f8496cb00d852d590d1f1.

--- BSTTiming-main/BSTTiming/Program.cs
+++ BSTTiming-main/BSTTiming/Program.cs
@@ -1,8 +1,6 @@
-using System;
-
 namespace BSTTiming
 {
-    class Program : Object
+    class Program : System.Object
     {
         /// <summary>
         /// Duration of one second
@@ -14,7 +12,7 @@ namespace BSTTiming
         static void Main(string[] args)
         {
             string line;
-using (StreamWriter file = new StreamWriter(@"C:\Users\Jesus Zarate\Desktop\timingResults.txt"))
+using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus Zarate\Desktop\timingResults.txt"))
             {
                 line = "Time";
                 Console.WriteLine(line);
