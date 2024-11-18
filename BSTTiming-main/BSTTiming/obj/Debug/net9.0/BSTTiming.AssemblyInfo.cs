afc46e0bf6de0a508b0498932c06d5af76e8f3a7
NoReply Amazon <no-reply@amazon.com>
2024-11-18 03:51:24 +0000

    [Commit] Iteration 220 #errors = 145 <= 144.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/BSTTiming-main/BSTTiming/Program.cs][CS0518][46,23] Predefined type 'System.Double' is not defined or imported``.

--- BSTTiming-main/BSTTiming/obj/Debug/net9.0/BSTTiming.AssemblyInfo.cs
+++ BSTTiming-main/BSTTiming/obj/Debug/net9.0/BSTTiming.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef97b051a47bc6894f762766a323241a2f0ad8ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02e863d9cb2e95352a623d45ecb484af40f43418")]
 [assembly: System.Reflection.AssemblyProductAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
