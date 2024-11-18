dfb7c1170f54a833a5d06461f7babdfe1cae53dc
NoReply Amazon <no-reply@amazon.com>
2024-11-18 11:01:49 +0000

    Revert "[Commit] Iteration 1302 #errors = 147 <= 147."
    This reverts commit 70293d44b42b54b74dc97876a59e45fc50c788e2.

--- BSTTiming-main/BSTTiming/Program.cs
+++ BSTTiming-main/BSTTiming/Program.cs
@@ -1,4 +1,4 @@
-global using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
--- BSTTiming-main/BSTTiming/obj/Debug/net9.0/BSTTiming.AssemblyInfo.cs
+++ BSTTiming-main/BSTTiming/obj/Debug/net9.0/BSTTiming.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e64190177ff0404990cd839dc91c645a23faed16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+659ca202c188b80c410393c49c007d6fbb25fc92")]
 [assembly: System.Reflection.AssemblyProductAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
