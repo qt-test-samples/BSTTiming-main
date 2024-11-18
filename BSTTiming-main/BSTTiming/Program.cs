0aa9690a53d564c54b8bbb3d7faa3149a222b799
NoReply Amazon <no-reply@amazon.com>
2024-11-18 11:14:50 +0000

    [Commit] Iteration 1334 #errors = 147 <= 148.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/BSTTiming-main/BSTTiming/Program.cs][CS0246][13,14] The type or namespace name 'System' could not be found (are you missing a using directive or an assembly reference?)``.

--- BSTTiming-main/BSTTiming/Program.cs
+++ BSTTiming-main/BSTTiming/Program.cs
@@ -10,7 +10,6 @@ using System.Threading.Tasks;
 using System.Numerics;
 using System.Globalization;
 using System.Runtime;
-using static System.Console;
 
 namespace BSTTiming
 {
@@ -29,7 +28,7 @@ namespace BSTTiming
 using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus Zarate\Desktop\timingResults.txt"))
             {
                 line = "Time";
-                Console.WriteLine(line);
+                System.Console.WriteLine(line);
                 file.WriteLine(line);
 
                 for (int i = 10; i <= 20; i++)
@@ -38,12 +37,12 @@ using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus
                     SIZE = size;
                     line = RunBSTTiming(size).ToString();
 
-                    Console.WriteLine(line);
+                    System.Console.WriteLine(line);
                     file.WriteLine(line);
                 }
             }
-            Console.WriteLine("Finished");
-            Console.Read();
+            System.Console.WriteLine("Finished");
+            System.Console.Read();
         }
 
         public static double RunBSTTiming(int size)
--- BSTTiming-main/BSTTiming/obj/Debug/net9.0/BSTTiming.AssemblyInfo.cs
+++ BSTTiming-main/BSTTiming/obj/Debug/net9.0/BSTTiming.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f10215bf30a59c68e40ffc7df52fd66d20f91a43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13fda15be2df6a98487a05b441baf0ff1bc1c584")]
 [assembly: System.Reflection.AssemblyProductAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BSTTiming")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
