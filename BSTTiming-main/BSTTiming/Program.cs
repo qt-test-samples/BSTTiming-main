1fea572d63bcf11f759b4f3cf6eee2608b429a49
NoReply Amazon <no-reply@amazon.com>
2024-11-18 07:21:57 +0000

    Revert "[Commit] Iteration 759 #errors = 139 <= 138."
    This reverts commit 98e712a16c048de1df0b37250925ec42feb56b34.

--- BSTTiming-main/BSTTiming/Program.cs
+++ BSTTiming-main/BSTTiming/Program.cs
@@ -6,7 +6,7 @@ using System.Linq;
 
 namespace BSTTiming
 {
-    class Program
+    class Program : System.Object
     {
         /// <summary>
         /// Duration of one second
@@ -18,7 +18,7 @@ namespace BSTTiming
         static void Main(string[] args)
         {
             string line;
-using (StreamWriter file = new StreamWriter(@"C:\Users\Jesus Zarate\Desktop\timingResults.txt"))
+using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus Zarate\Desktop\timingResults.txt"))
             {
                 line = "Time";
                 Console.WriteLine(line);
@@ -126,7 +126,7 @@ using (StreamWriter file = new StreamWriter(@"C:\Users\Jesus Zarate\Desktop\timi
 
                 bst.Add(number);
             }
-
+            
             return bst;
         }
 
@@ -137,5 +137,6 @@ using (StreamWriter file = new StreamWriter(@"C:\Users\Jesus Zarate\Desktop\timi
         {
             return (((double)sw.ElapsedTicks) / Stopwatch.Frequency) * 1000;
         }
+
     }
 }
