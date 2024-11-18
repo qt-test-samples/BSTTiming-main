6a6f932031a85a9285efc5201fb5cab7f9d3b0d2
NoReply Amazon <no-reply@amazon.com>
2024-11-18 06:12:46 +0000

    Revert "[Commit] Iteration 587 #errors = 146 <= 146."
    This reverts commit efce21beb8e7c64bd79ba7759927f1978f005392.

--- BSTTiming-main/BSTTiming/Program.cs
+++ BSTTiming-main/BSTTiming/Program.cs
@@ -120,9 +120,9 @@ namespace BSTTiming
             return set.ToArray();
         }
 
-        private static System.Collections.Generic.SortedSet<int> generateTree(int size)
+        private static SortedSet<int> generateTree(int size)
         {
-            System.Collections.Generic.SortedSet<int> bst = new System.Collections.Generic.SortedSet<int>();
+            SortedSet<int> bst = new SortedSet<int>();
             Random random = new Random();
 
             int number;
@@ -135,7 +135,7 @@ namespace BSTTiming
 
                 bst.Add(number);
             }
-
+            
             return bst;
         }
 
