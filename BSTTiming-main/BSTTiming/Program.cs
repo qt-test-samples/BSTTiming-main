eec7e7db021551ea8bea364728606c2d69196371
NoReply Amazon <no-reply@amazon.com>
2024-11-18 09:37:05 +0000

    Revert "[Commit] Iteration 1084 #errors = 142 <= 142."
    This reverts commit 0be0538c9b5856eee0c25d4b72ef85c9b363e593.

--- BSTTiming-main/BSTTiming/Program.cs
+++ BSTTiming-main/BSTTiming/Program.cs
@@ -25,20 +25,20 @@ namespace BSTTiming
 using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Jesus Zarate\Desktop\timingResults.txt"))
             {
                 line = "Time";
-                System.Console.WriteLine(line);
+                Console.WriteLine(line);
                 file.WriteLine(line);
 
                 for (int i = 10; i <= 20; i++)
                 {
-                    SIZE = (int)System.Math.Pow(2, i);
+                    SIZE = (int)Math.Pow(2, i);
                     line = RunBSTTiming(SIZE).ToString();
 
-                    System.Console.WriteLine(line);
+                    Console.WriteLine(line);
                     file.WriteLine(line);
                 }
             }
-            System.Console.WriteLine("Finished");
-            System.Console.Read();
+            Console.WriteLine("Finished");
+            Console.Read();
         }
 
         public static double RunBSTTiming(int size)
