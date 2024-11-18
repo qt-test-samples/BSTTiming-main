8460a6b7a6c847d8437300bcf639f59a36bb8beb
NoReply Amazon <no-reply@amazon.com>
2024-11-18 10:23:28 +0000

    Revert "[Commit] Iteration 1205 #errors = 149 <= 149."
    This reverts commit 100da5371e014f946fb3fede5cc18cc5303a4f6d.

--- BSTTiming-main/BSTTiming/Program.cs
+++ BSTTiming-main/BSTTiming/Program.cs
@@ -17,14 +17,14 @@ namespace BSTTiming
 {
 using System;
 
-    class Program
+    class Program : System.Object
     {
         /// <summary>
         /// Duration of one second
         /// </summary>
         public const int DURATION = 1000;
 
-        public static int SIZE = 0;
+        public static System.Int32 SIZE = 0;
 
         static void Main(string[] args)
         {
